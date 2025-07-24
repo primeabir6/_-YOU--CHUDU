5f58daaa7a9920615db7eb95f0462ee2291b4fac
primebot3030 <mdabirga760@gmail.com>
2025-07-24 21:51:02 +0600

    Create chudu.js

---
+++ Script/commands/chudu.js
@@ -0,0 +1,18 @@
+module.exports.config = {
+  name: "chudu",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "—͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  description: "us chudu reply fanny text",
+  commandCategory: "fun",
+  usages: "/chudu",
+  cooldowns: 3,
+};
+
+module.exports.run = async ({ api, event }) => {
+  return api.sendMessage(
+    "এটা বললে তুই কি চাস আমি তোর girlfriend রে নিয়া পালাই যাই😏",
+    event.threadID,
+    event.messageID
+  );
+};
