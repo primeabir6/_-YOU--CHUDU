4e8484f16b88d496084a9f5f98fe0a7dcf0e37b3
primebot3030 <mdabirga760@gmail.com>
2025-07-25 16:39:32 +0600

    Update chudi.js

--- Script/commands/chudi.js
+++ Script/commands/chudi.js
@@ -3,7 +3,7 @@
   version: "1.0.0",
   hasPermssion: 0,
   credits: "—͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-  description: "us chudu reply fanny text",
+  description: "us chudi reply fanny text",
   commandCategory: "fun",
   usages: "/chudi",
   cooldowns: 3,
@@ -11,7 +11,7 @@
 
 module.exports.run = async ({ api, event }) => {
   return api.sendMessage(
-    "খানকির পোলা তোকে চুদি 🥵",
+    "খানকির পোলা তোকে চুদি 🥵""খানকির পোলা তোরে চুদি🫦",
     event.threadID,
     event.messageID
   );
